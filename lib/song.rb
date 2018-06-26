fafdf5b465a2b2e9d9b6b12644b43e3183b660ca
David Hopkins <dlhopkins2@gmail.com>
2018-06-26 22:00:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,14 +34,6 @@ class Song
     self.all.detect {|song| song.name == name}
   end
 
-  def self.find_or_create_by_name(name)
-    if find_by_name.nil?
-      self.create(name)
-    else
-      find_by_name(name)
-    end
-  end
-
   def self.all
     @@all
   end
