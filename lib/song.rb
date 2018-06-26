606b859684ba9a118168ef91c783bbed174f9299
David Hopkins <dlhopkins2@gmail.com>
2018-06-26 21:51:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,10 +35,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name == false
-      return song.name = name
-    else
+    if find_by_name.nil?
       self.create(name)
+    else
+      find_by_name(name)
     end
   end
 
